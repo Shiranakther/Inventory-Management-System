3de0b84dab5dda3331145e93953861cd1ee60c9b
shiran <akthermhs@gmail.com>
2023-11-30 23:58:17 +0530

    possystem

---
+++ pos/src/pos/Pos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pos;
+
+public class Pos {
+
+    
+    public static void main(String[] args) {
+    }
+    
+}
