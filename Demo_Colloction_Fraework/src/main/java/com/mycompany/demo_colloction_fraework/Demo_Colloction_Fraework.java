988437eb2eae6b2dc6dd7ea2c6da85f455eef264
shiran <akthermhs@gmail.com>
2024-01-04 03:03:01 +0530

    update

---
+++ Demo_Colloction_Fraework/src/main/java/com/mycompany/demo_colloction_fraework/Demo_Colloction_Fraework.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demo_colloction_fraework;
+
+/**
+ *
+ * @author shiran
+ */
+public class Demo_Colloction_Fraework {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
